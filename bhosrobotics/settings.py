9ca189a1b755ce53eba573582f40efdbba85813c
Asus <Asus@DESKTOP-99DNRP8>
2020-11-09 17:57:21 +0400

    some changess

--- bhosrobotics/settings.py
+++ bhosrobotics/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
